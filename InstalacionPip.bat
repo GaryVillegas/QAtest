0df5376c1acbebbbdd4fb8c8b05df5c9075a5ba0
Gary Villegas <gary.villegasb@gmail.com>
2024-10-09 12:28:53 -0300

    Se agrega instalador

---
+++ InstalacionPip.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Instalacion de paquetes pip
+
+pip install django pypdf pillow pandas pycparser plotly numpy crispy crispy-bootstrap5 openpyxl xhtml2pdf reportlab
+
+echo.
+echo Instalacion completada. Por favor, revise la salida para verificar que no hubo errores.
+pause
